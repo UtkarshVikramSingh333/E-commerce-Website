1aa3f2f0bb45ed4ad5dcbfab6ac7856c1b49b0cb
UtkarshVikramSingh333 <utkarshvikramsingh333@gmail.com>
2024-04-26 03:04:11 +0530

    first commit

---
+++ index/script.js
@@ -0,0 +1,17 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+
+if(bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+    })
+
+}
+if(close){
+    bar.addEventListener('click', () =>{
+        nav.classList.remove('active');
+    })
+
+}
